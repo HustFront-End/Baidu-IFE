c970242616064219070176b33ded2167908f0b28
hucece <2578285142@qq.com>
2016-04-19 15:42:59 +0800

    修改util.js

--- The Second Stage/Task_Hucece/js/util.js
+++ The Second Stage/Task_Hucece/js/util.js
@@ -1,6 +1,7 @@
 
 	var EventUtil = {
 		addHandler: function(element, type, handler){
+
 			if(element.addEventListener){
 				element.addEventListener(type, handler, false);
 			}else if(element.attachEvent){
@@ -8,15 +9,42 @@
 			}else{
 				element["on" + type] = handler;
 			}
+
 		},
 
 		removeHandler: function(element, type, handler){
+
 			if(element.removeEventListener){
 				element.removeEventListener(type, handler, false);
 			}else if(element.detachEvent){
 				element.detachEvent("on" + type, handler);
 			}else{
 				element["on" + type] = null;
+			}
+
+		},
+
+		getEvent: function(event){
+
+			return event?event : window.event;
+
+		},
+
+		stopEventBubble: function(event){
+
+			if(event.stopPropagation){
+				event.stopPropagation();
+			}else{
+				event.cancelBubble = true;
+			}
+		},
+
+		getEventTarget: function(event){
+
+			if(event.target){
+				return event.target;
+			}else{
+				return event.srcElement;
 			}
 		}
 	};
